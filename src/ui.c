2fd23c7b7d2f023d3f08dbf8646aec7beff8c015
defskela <sashokafanas@gmail.com>
2025-01-30 15:34:29 +0300

    [+] Созданы команды для управления интерфейсом
    R: взаимодействие пользователя с программой
    
    FB: консоль, текст

---
+++ src/ui.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "ui.h"
+
+void show_menu()
+{
+    printf("\nТекстовый редактор\n");
+    printf("1. Открыть файл\n");
+    printf("2. Просмотреть текст\n");
+    printf("3. Добавить строку\n");
+    printf("4. Удалить строку\n");
+    printf("5. Изменить строку\n");
+    printf("6. Сохранить файл\n");
+    printf("7. Выйти\n");
+    printf("Выберите действие: ");
+}
+
+int get_user_command()
+{
+    int command;
+    scanf("%d", &command);
+    return command;
+}
+
+void print_lines(char **lines, int line_count)
+{
+    if (line_count == 0)
+    {
+        printf("\nВы не выбрали файл или файл пуст\n");
+        return;
+    }
+
+    printf("\nТекущий текст\n");
+    for (int i = 0; i < line_count; i++)
+    {
+        printf("%d. %s\n", i, lines[i]);
+    }
+}
---
+++ src/ui.h
@@ -0,0 +1,8 @@
+#ifndef UI_H
+#define UI_H
+
+void show_menu();
+int get_user_command();
+void print_lines(char **lines, int line_count);
+
+#endif
